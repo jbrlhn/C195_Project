cc3801beceb40f22f170364d9a8bce55bcda8ff7
Jon <jbarlahan@costar.com>
2021-03-13 18:05:26 -0800

    - Added fxid for username and password - Created methods signIn and Exit -Get username and password text

--- src/Controller/LoginMenuController.java
+++ src/Controller/LoginMenuController.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+
 public class LoginMenuController {
+
+    public TextField userNameText;
+    public PasswordField passwordText;
+
+    public void signInAction(ActionEvent actionEvent) {
+
+        String message = userNameText.getText();
+        String password = passwordText.getText();
+        System.out.println(message);
+        System.out.println(password);
+    }
+
+    public void exitAction(ActionEvent actionEvent) {
+        System.out.println("Exit");
+    }
 }
